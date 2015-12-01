eb472a869188303a9d25a8e3998eee1dcb53d288
tektx <tektx@users.noreply.github.com>
2015-11-30 23:30:27 -0600

    Add function to recursively get ancestor node
    Create function to get 'parent of parent of parent', etc., and highlight
    the element to clean up long string of parentNodes

--- AmazonWishlistPriceDrop.user.js
+++ AmazonWishlistPriceDrop.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Amazon Wishlist Price Drop
 // @author         T. Knight
-// @description    Highlights wishlist items that have dropped 20% or more in price
-// @version        4
+// @description    Highlights wishlist items that have dropped substantially in price or are unavailable
+// @version        5
 // @include          /https?://www\.amazon\.com.*/
 // ==/UserScript==
 
@@ -11,39 +11,52 @@ var hidePrices = 0;
 
 // Hide Price function
 if(hidePrices) {
-    var vPrices = document.getElementsByTagName("*");
+    var vPrices = document.getElementsByTagName('*');
     for(var i=0; i<vPrices.length; i++) {
         if((/^\$.*/).test(vPrices[i].innerHTML)) {
-            vPrices[i].innerText = "N/A";
+            vPrices[i].innerText = 'N/A';
         }
     }
 }
 
-var thisPage = "";
+var thisPage = '';
 
 setInterval(function() {
-    if(window.location.href != thisPage) {
-        var thisPage = window.location.href;
-		var vItemsPriceDrop = document.getElementsByClassName("a-row itemPriceDrop");
-		var vItemsUnavailable = document.getElementsByClassName("a-size-base a-color-price a-text-bold");
-		var vItemsUnavailable2 = document.getElementsByClassName("itemAvailMessage a-text-bold");
+  if(window.location.href != thisPage) {
+      thisPage = window.location.href;
 
-		for(var i=0; i<vItemsPriceDrop.length; i++) {
-			if(vItemsPriceDrop[i].innerText.match(/Price dropped [2-9][0-9]/)) {
-				vItemsPriceDrop[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.backgroundColor = 'lightgreen';
-			}
-		}
+    var vItemsPriceDrop = document.getElementsByClassName('a-row itemPriceDrop'),
+        vItemsUnavailable1 = document.getElementsByClassName('a-size-base a-color-price a-text-bold'),
+        vItemsUnavailable2 = document.getElementsByClassName('itemAvailMessage a-text-bold');
 
-		for(var i=0; i<vItemsUnavailable.length; i++) {
-			if(vItemsUnavailable[i].innerText.match("Unavailable")) {
-				vItemsUnavailable[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.backgroundColor = '#ffdada';
-			}
-		}
+    // Item has dropped 20% or more in price
+    for(var i=0; i<vItemsPriceDrop.length; i++) {
+      if(vItemsPriceDrop[i].innerText.match(/Price dropped [2-9][0-9]/)) {
+        colorElement(vItemsPriceDrop[i], 8, 'lightgreen');
+      }
+    }
 
-		for(var i=0; i<vItemsUnavailable2.length; i++) {
-			if(vItemsUnavailable2[i].innerText.match("unavailable")) {
-				vItemsUnavailable2[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.backgroundColor = '#ffdada';
-			}
-		}
-	}
+    // Item is 'Currently unavailable'
+    for(var j=0; j<vItemsUnavailable1.length; j++) {
+      if(vItemsUnavailable1[j].innerText.toLowerCase().match("unavailable")) {
+        colorElement(vItemsUnavailable1[j], 10, '#ffdada');
+      }
+    }
+
+    // Item is 'Unavailable'
+    for(var k=0; k<vItemsUnavailable2.length; k++) {
+      if(vItemsUnavailable2[k].innerText.toLowerCase().match("unavailable")) {
+        colorElement(vItemsUnavailable2[k], 10, '#ffdada');
+      }
+    }
+  }
 }, 2000);
+
+function colorElement(node, number, color) {
+  if(number > 0) {
+    console.log("number != 0");
+    colorElement(node.parentNode, number-1, color);
+  } else {
+    node.style.backgroundColor = color;
+  }
+}
